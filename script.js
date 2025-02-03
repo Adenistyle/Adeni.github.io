bf929c147ce6a6ff8b47d5d2fe3d66e21a99b7d8
Adenistyle <adenistylefrance@gmail.com>
2025-02-03 15:17:05 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+
+document.getElementById('inscriptionForm').addEventListener('submit', function(e) {
+    e.preventDefault();
+    alert("Merci pour votre inscription !");
+    // Code pour envoyer les données du formulaire ici (ex: AJAX ou redirection)
+});
